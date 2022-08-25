f4f550b8e4b00d1d97e7ff59aeaae321afd6cfcd
purushartha <46944757+purusharthasingh@users.noreply.github.com>
2022-08-25 11:47:51 -0400

    Added comments

--- correlations.py
+++ correlations.py
@@ -3,35 +3,40 @@ import pandas as pd
 import os
 import shutil
 
-homedir = '/home/nlp/pxs288/SU21/PyrEval/PyrEval_0615/'
-resdir = homedir+'Results'
-baselinedir = homedir + 'Baseline'
-data = 'av-abcd'
+homedir = os.getcwd()
+resdir = os.path.join(homedir, 'Results')
+baselinedir = os.path.join(homedir, 'Baseline')
+# Name of the reference dataset you want to use
+data = 'cc'
+input_csv = ''
 
-
+# Can iteratively go through datasets but the if statement limits to one dataset
 for dataset in os.listdir(baselinedir):
 	if dataset != data:
 		continue
+		
 	df_corr = pd.DataFrame(columns = ['TopK', 'SortingMetric', 'WeightingMetric', 'WeightingScheme', 'CorrelationQuality', 'CorrelationCoverage','CorrelationComprehensive'])
 	f = os.listdir(os.path.join(baselinedir, dataset))[0]
 	df_baseline = pd.read_csv(os.path.join(baselinedir, dataset, f))
 	df_baseline.sort_values(by=['filename'], inplace=True, ignore_index=True)
 	# print(df_baseline)
-
+	# Need to replace the dataframe below with the csv that needs to be read
 	for config in os.listdir(os.path.join(resdir, dataset)):
 		if not os.path.isdir(os.path.join(resdir, dataset, config)):
 			continue
+		# The input results which we calculate the correlations to annotations for
 		f = os.path.join(resdir, dataset, config, 'results.csv')
 		df_conf = pd.read_csv(f, skiprows = 1)
 		df_conf.sort_values(by=['Summary'], inplace=True, ignore_index=True)
 		# print(df_conf)
 		# exit()
-
+		# This was done to check configurations of multiple parameter values saved as directories
 		topk, sort, weight, scheme = config.split('_')
 		con_ql = df_conf['quality']
 		con_co = df_conf['coverage']
 		con_comp = df_conf['Comprehensive']
 
+		# Code specific to each of the benchmark datasets
 		if data in ['av', 'cc', 'plb', 'aied', 'plb-abcd', 'cc-abcd', 'av-abcd']:
 			bl_ql = df_baseline['qualityScore']
 			bl_co = df_baseline['coverageScore']
