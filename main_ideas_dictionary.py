4b52bfb42241493d0ed08224e53a757c78c9d15a
Becky Passonneau <rjp49@cse.psu.edu>
2023-04-07 13:55:16 -0400

    Add files via upload
    Updated main_ideas_dictionary.py to ensure dictionary works with 8 content units for the second pyramid

--- main_ideas_dictionary.py
+++ main_ideas_dictionary.py
@@ -3,7 +3,7 @@ import os
 
 import re
 
-#Written by Mahsa (02-17-2023)
+#Written by Mahsa (04-06-2023)
 def replace_all(repls, str):
     # return re.sub('|'.join(repls.keys()), lambda k: repls[k.group(0)], str)
     return re.sub('|'.join(re.escape(key) for key in repls.keys()),
@@ -18,7 +18,11 @@ def reorder_cu_vectors(cu_df,enotebook_cu_vectors_path,scu_mapping_list,essay_ma
     reordered_cu_df = reordered_cu_df[reordered_cu_df.columns[scu_mapping_list_int ]]
 
     #This changes the dictionary order
-    reordered_cu_df.rename(columns={scu_mapping_list_int[0]: essay_main_ideas_list[0], scu_mapping_list_int[1]: essay_main_ideas_list[1], scu_mapping_list_int[2]: essay_main_ideas_list[2], scu_mapping_list_int[3]: essay_main_ideas_list[3], scu_mapping_list_int[4]: essay_main_ideas_list[4], scu_mapping_list_int[5]: essay_main_ideas_list[5]}, inplace=True)
+    # reordered_cu_df.rename(columns={scu_mapping_list_int[0]: essay_main_ideas_list[0], scu_mapping_list_int[1]: essay_main_ideas_list[1], scu_mapping_list_int[2]: essay_main_ideas_list[2], scu_mapping_list_int[3]: essay_main_ideas_list[3], scu_mapping_list_int[4]: essay_main_ideas_list[4], scu_mapping_list_int[5]: essay_main_ideas_list[5]}, inplace=True)
+    for col in reordered_cu_df.columns:
+        i = 0
+        reordered_cu_df.rename(columns={col:str(col).replace(str(scu_mapping_list_int[i]),str(essay_main_ideas_list[i]))}, inplace=True)
+        i = i+1
 
     reordered_cu_df.to_csv(enotebook_cu_vectors_path)
 
@@ -32,6 +36,10 @@ def write_logs_to_file(data, write_path, filename):
 
 def replace_log(log_path,elog_path,scu_mapping_list,essay_main_ideas_list,is_essay_one):
     scu_mapping_list_int = [eval(i) for i in scu_mapping_list]
+    if is_essay_one:
+        number_of_cus = 6
+    else:
+        number_of_cus = 8
 
     for filename in os.listdir(log_path):
 
@@ -41,6 +49,12 @@ def replace_log(log_path,elog_path,scu_mapping_list,essay_main_ideas_list,is_ess
             write_path = elog_path
 
             listi = data.split('\n')
+
+
+            for i in range(0,len(listi)):
+                if "==================" in listi[i]:
+                    listi[i]=listi[i].replace(listi[i],"\n")
+
             content_unit_list_split = listi[14].split(': ')
 
             #Has no content units
@@ -65,13 +79,10 @@ def replace_log(log_path,elog_path,scu_mapping_list,essay_main_ideas_list,is_ess
                 if l[-1] != ' ':
                     listi2.append(int(l[-1]))
 
-            # Change this part to change the dictionary order
-            if scu_mapping_list_int[0] in listi2: listi3.append(essay_main_ideas_list[0])
-            if scu_mapping_list_int[1] in listi2: listi3.append(essay_main_ideas_list[1])
-            if scu_mapping_list_int[2] in listi2: listi3.append(essay_main_ideas_list[2])
-            if scu_mapping_list_int[3] in listi2: listi3.append(essay_main_ideas_list[3])
-            if scu_mapping_list_int[4] in listi2: listi3.append(essay_main_ideas_list[4])
-            if scu_mapping_list_int[5] in listi2: listi3.append(essay_main_ideas_list[5])
+
+            for i in range(0,number_of_cus):
+                if scu_mapping_list_int[i] in listi2: listi3.append(essay_main_ideas_list[i])
+
 
 
 
@@ -96,6 +107,7 @@ def replace_log(log_path,elog_path,scu_mapping_list,essay_main_ideas_list,is_ess
 
             data = listi4
 
+
             # This part changes the dictionary order
             search_text1 = "Content Unit: "+ scu_mapping_list[0]+" "
             replace_text1 = "Content Unit: " + essay_main_ideas_list[0]+" "
@@ -115,14 +127,33 @@ def replace_log(log_path,elog_path,scu_mapping_list,essay_main_ideas_list,is_ess
             search_text6 = "Content Unit: "+ scu_mapping_list[5]+" "
             replace_text6 = "Content Unit: "+ essay_main_ideas_list[5]+" "
 
-            data = replace_all({search_text1: replace_text1, search_text2: replace_text2, search_text3: replace_text3,
+            if is_essay_one:
+                if "Content Unit: 6" in data:
+                    data = replace_all({"Content Unit: 6":"Content Unit: 20"},data)
+                data = replace_all({search_text1: replace_text1, search_text2: replace_text2, search_text3: replace_text3,
                                 search_text4: replace_text4, search_text5: replace_text5, search_text6: replace_text6},
                                data)
+            else:
+                if "Content Unit: 8" in data:
+                    data = replace_all({"Content Unit: 8": "Content Unit: 20"},data)
+                search_text7 = "Content Unit: " + scu_mapping_list[6] + " "
+                replace_text7 = "Content Unit: " + essay_main_ideas_list[6] + " "
+
+                search_text8 = "Content Unit: " + scu_mapping_list[7] + " "
+                replace_text8 = "Content Unit: " + essay_main_ideas_list[7] + " "
+                data = replace_all({search_text1: replace_text1, search_text2: replace_text2, search_text3: replace_text3,
+                                search_text4: replace_text4, search_text5: replace_text5, search_text6: replace_text6,search_text7: replace_text7,search_text8: replace_text8},
+                               data)
+
+
+
 
         data = data.replace('Sentence:','%Sentence:')
         sp_by_sent = data.split('%')
         sp_by_seg = []
         final = []
+
+
 
 
         for sp in sp_by_sent:
@@ -136,19 +167,30 @@ def replace_log(log_path,elog_path,scu_mapping_list,essay_main_ideas_list,is_ess
             temp2 = ''
             for sg_in in sg:
                 if 'Content Unit:' in sg_in and 'None' not in sg_in:
+
                     temp = sg_in.split(' ')[6]
-                    if int(temp) < 6:
+
+                    if int(temp) <= number_of_cus:
+
                         if flag is False:
                             flag = True
                             final.append(temp2)
-                            final.append(sg_in)
+                        final.append(sg_in)
+                        # print(sg_in)
+                        # print("@@")
 
+                elif 'Content Unit:' in sg_in and 'None' in sg_in:
+                    pass
                 else:
                     temp2 = sg_in
+                    # print(sg_in)
+                    # print("^^")
 
 
         temp = str(final[0][0])
         final.pop(0)
+        # print(final)
+
         # data = ''.join(temp)
         data = ''.join(str(f) for f in final)
         data= temp + data
